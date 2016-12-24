3bab6fea426e015a41136812d1ad0f6d5e82c434
rtsonneveld <r.t.sonneveld@gmail.com>
2016-12-25 00:06:29 +0100

    Made Float Math class public Changed Float Math class to use expression bodied functions because it's hip

--- RaymanRebuilt/RREngine/Utils/MathF.cs
+++ RaymanRebuilt/RREngine/Utils/MathF.cs
@@ -6,32 +6,32 @@ using System.Threading.Tasks;
 
 namespace RREngine.Utils
 {
-    static class Mathf
+    public static class Mathf
     {
-        public static float Acos(float d) { return (float)Math.Acos(d); }
-        public static float Asin(float d) { return (float)Math.Asin(d); }
-        public static float Atan(float d) { return (float)Math.Atan(d); }
-        public static float Atan2(float y, float x) { return (float)Math.Atan2(y, x); }
-        public static float Ceiling(float a) { return (float)Math.Ceiling(a); }
-        public static float Cos(float d) { return (float)Math.Cos(d); }
-        public static float Cosh(float value) { return (float)Math.Cosh(value); }
-        public static float Exp(float d) { return (float)Math.Exp(d); }
-        public static float Floor(float d) { return (float)Math.Floor(d); }
-        public static float IEEERemainder(float x, float y) { return (float)Math.IEEERemainder(x, y); }
-        public static float Log(float d) { return (float)Math.Log(d); }
-        public static float Log(float a, float newBase) { return (float)Math.Log(a, newBase); }
-        public static float Log10(float d) { return (float)Math.Log10(d); }
-        public static float Min(float val1, float val2) { return (float)Math.Min(val1, val2); }
-        public static float Pow(float x, float y) { return (float)Math.Pow(x, y); }
-        public static float Round(float a) { return (float)Math.Round(a); }
-        public static float Round(float value, MidpointRounding mode) { return (float)Math.Round(value, mode); }
-        public static float Round(float value, int digits) { return (float)Math.Round(value, digits); }
-        public static float Round(float value, int digits, MidpointRounding mode) { return (float)Math.Round(value, digits, mode); }
-        public static float Sin(float a) { return (float)Math.Sin(a); }
-        public static float Sinh(float value) { return (float)Math.Sinh(value); }
-        public static float Sqrt(float d) { return (float)Math.Sqrt(d); }
-        public static float Tan(float a) { return (float)Math.Tan(a); }
-        public static float Tanh(float value) { return (float)Math.Tanh(value); }
-        public static float Truncate(float d) { return (float)Math.Truncate(d); }
+        public static float Acos(float d) =>(float)Math.Acos(d); 
+        public static float Asin(float d) =>(float)Math.Asin(d); 
+        public static float Atan(float d) =>(float)Math.Atan(d); 
+        public static float Atan2(float y, float x) =>(float)Math.Atan2(y, x); 
+        public static float Ceiling(float a) =>(float)Math.Ceiling(a); 
+        public static float Cos(float d) =>(float)Math.Cos(d); 
+        public static float Cosh(float value) =>(float)Math.Cosh(value); 
+        public static float Exp(float d) =>(float)Math.Exp(d); 
+        public static float Floor(float d) =>(float)Math.Floor(d); 
+        public static float IEEERemainder(float x, float y) =>(float)Math.IEEERemainder(x, y); 
+        public static float Log(float d) =>(float)Math.Log(d); 
+        public static float Log(float a, float newBase) =>(float)Math.Log(a, newBase); 
+        public static float Log10(float d) =>(float)Math.Log10(d); 
+        public static float Min(float val1, float val2) =>(float)Math.Min(val1, val2); 
+        public static float Pow(float x, float y) =>(float)Math.Pow(x, y); 
+        public static float Round(float a) =>(float)Math.Round(a); 
+        public static float Round(float value, MidpointRounding mode) =>(float)Math.Round(value, mode); 
+        public static float Round(float value, int digits) =>(float)Math.Round(value, digits); 
+        public static float Round(float value, int digits, MidpointRounding mode) =>(float)Math.Round(value, digits, mode); 
+        public static float Sin(float a) =>(float)Math.Sin(a); 
+        public static float Sinh(float value) =>(float)Math.Sinh(value); 
+        public static float Sqrt(float d) =>(float)Math.Sqrt(d); 
+        public static float Tan(float a) =>(float)Math.Tan(a); 
+        public static float Tanh(float value) =>(float)Math.Tanh(value); 
+        public static float Truncate(float d) =>(float)Math.Truncate(d); 
     }
 }
