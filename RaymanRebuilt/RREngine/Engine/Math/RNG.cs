b3e88af6c36fe78681979eea00a65dc9378cc902
rtsonneveld <r.t.sonneveld@gmail.com>
2017-02-04 16:26:10 +0100

    + Renamed Random to Rng + Added a GetDefaultRng singleton function

--- RaymanRebuilt/RREngine/Engine/Math/RNG.cs
+++ RaymanRebuilt/RREngine/Engine/Math/RNG.cs
@@ -5,39 +5,52 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace RREngine.Engine.Math {
-  class Random {
+  public class Rng {
+
+    static Rng defaultRng;
+
+    public static Rng GetDefaultRng() {
+      if (defaultRng == null) {
+        defaultRng = new Rng();
+      }
+      return defaultRng;
+    }
 
     System.Random sysRandom;
 
-    public Random() {
+    public Rng() {
       this.sysRandom = new System.Random();
     }
 
-    public Random(int seed) {
+    public Rng(int seed) {
       this.sysRandom = new System.Random(seed);
     }
 
-    public bool getBool() {
+    public void Seed(int seed) {
+      this.sysRandom = new System.Random(seed);
+    }
+
+    public bool GetBool() {
       return this.sysRandom.Next() % 2 == 0;
     }
 
-    public int getInt(int max) {
+    public int GetInt(int max) {
       return this.sysRandom.Next(max);
     }
 
-    public int getInt(int a, int b) {
+    public int GetInt(int a, int b) {
       return this.sysRandom.Next(a, b);
     }
 
-    public byte getByte() {
+    public byte GetByte() {
       return (byte) (this.sysRandom.Next() % 256);
     }
 
-    public float getFloat(float min, float max) {
+    public float GetFloat(float min, float max) {
       return min + (float)(this.sysRandom.NextDouble() * (max - min));
     }
 
-    public float getFloat(float max) {
+    public float GetFloat(float max) {
       return (float)this.sysRandom.NextDouble() * max;
     }
 
